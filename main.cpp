90dea7450d37447d69803a345a2af46f60a7c91f
DevBarik731 <barikbiswajit731@gmail.com>
2026-08-09 21:17:08 +0530

    Corrected Rook

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@ int main(){
     Board a=Board();
     a.display();
     cout<<"\n";
+
     cout<<King_validate(a,7,4,6,4)<<endl;
     cout<<Knight_validate(a,7,1,5,0)<<endl;
+    cout<<Rook_validate(a,7,0,7,5)<<endl;
 
 }
--- rook.cpp
+++ rook.cpp
@@ -8,13 +8,19 @@ using namespace std;
 // 2 : possible with kill
 // move from (a,b) -> (x,y)
 int Rook_validate(Board &v,int a,int b,int x,int y){
-
     if(a!=x && b!=y) return 0;
-    if(v.board[x][y]*v.board[x][y]>0) return 0; // checking of same pairity
+    int step_x;
+    int step_y;
+    if(x>=a) step_x=1;
+    else step_x=-1;
+    if(y>=b) step_y=1;
+    else step_y=-1;
+
+    if(v.board[x][y]*v.board[a][b]>0) return 0; // checking of same pairity
 
     // rook only moves in empty straight lane, here we are checking if the lane is empty or not
-    for(size_t i=a+1;i<x;i++) if(v.board[i][b]) return 0;
-    for(size_t i=b+1;i<y;i++) if(v.board[a][i]) return 0;
+    for(int i=a+step_x;(step_x>0 && i<x) || (step_x<0 && i>x);i+=step_x) if(v.board[i][b]) return 0;
+    for(int i=b+step_y;(step_y>0 && i<y) || (step_y<0 && i>y);i+=step_y) if(v.board[a][i]) return 0;
     if(v.board[x][y]) return 2;
     else return 1;
 

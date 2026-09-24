3a8b393e389e55ff6fbbc1cf45e85a5d79001ff1
DevBarik731 <barikbiswajit731@gmail.com>
2026-09-25 02:37:30 +0530

    minor fix

--- src/minimax.hpp
+++ src/minimax.hpp
@@ -8,5 +8,5 @@ void knight_moves(Board &v,int x_king,int y_king,int x,int y,vector<vector<int>>
 void king_moves(Board &v,int x_king,int y_king,int x,int y,vector<vector<int>> &ds);
 void pawn_moves(Board &v,int x_king,int y_king,int x,int y,vector<vector<int>> &ds);
 vector<vector<int>> generateMoves(Board &v,int x_king,int y_king);
-int minimax(Board &v,int depth,bool player);
+int minimax(Board &v,int depth,int player);
 vector<int> best_move(Board &v,int x_king,int y_king);
